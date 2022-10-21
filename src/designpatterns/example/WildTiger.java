23575bb143d8fdcf2dc183c940201240a4614e32
Daniel <73000732+MonnTS@users.noreply.github.com>
2022-10-21 10:11:36 +0200

    ~

--- src/designpatterns/example/WildTiger.java
+++ src/designpatterns/example/WildTiger.java
@@ -3,6 +3,6 @@ package designpatterns.example;
 public class WildTiger implements ITiger {
     @Override
     public void aboutMe() {
-        System.out.println("I am wild tiger!");
+        System.out.println("I am a wild tiger!");
     }
 }
